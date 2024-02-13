7c9dc8ed5991f8846d0a1edb60a5183c88b69604
Xin Lu <xin@fortue.com>
2024-02-13 15:30:44 +0800

    Add a gatekeeper to migrate friendship to hbase

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,4 +1,5 @@
 from accounts.api.serializers import UserSerializerForFriendship
+from accounts.services import UserService
 from friendships.models import Friendship
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -6,49 +7,51 @@ from django.contrib.auth.models import User
 from friendships.services import FriendshipsService
 
 
-class FollowingUserIdSetMixin:
-    @property
-    def following_user_id_set(self: serializers.ModelSerializer):
+class BaseFriendshipSerializer(serializers.Serializer):
+    user = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    has_followed = serializers.SerializerMethodField()
+
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
+    def get_user_id(self, obj):
+        raise NotImplementedError
+
+    def _get_following_user_id_set(self):
         if self.context['request'].user.is_anonymous:
             return {}
         if hasattr(self, '_cached_following_user_id_set'):
             return self._cached_following_user_id_set
-        following_user_id_set = FriendshipsService.get_following_user_id_set(
-            self.context['request'].user.id
+
+        user_id_set = FriendshipsService.get_following_user_id_set(
+            self.context['request'].user.id,
         )
-        setattr(self, '_cached_following_user_id_set', following_user_id_set)
-        return following_user_id_set
-
-
-# 可以通过 source=xxx 指定去访问每个 model instance 的 xxx 方法
-# 即 model_instance.xxx 来获得数据
-# https://www.django-rest-framework.org/api-guide/serializers/#specifying-fields-explicitly
-class FollowerSerializer(serializers.ModelSerializer, FollowingUserIdSetMixin):
-    user = UserSerializerForFriendship(source='cached_from_user')
-    has_followed = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Friendship
-        fields = ('user', 'created_at', 'has_followed')
+        setattr(self, '_cached_following_user_id_set', user_id_set)
+        return user_id_set
 
     def get_has_followed(self, obj):
-        if self.context['request'].user.is_anonymous:
-            return False
-        return obj.from_user_id in self.following_user_id_set
+        return self.get_user_id(obj) in self._get_following_user_id_set()
+
+    def get_user(self, obj):
+        user = UserService.get_user_by_id(self.get_user_id(obj))
+        return UserSerializerForFriendship(user).data
+
+    def get_created_at(self, obj):
+        return obj.created_at
 
 
-class FollowingSerializer(serializers.ModelSerializer, FollowingUserIdSetMixin):
-    user = UserSerializerForFriendship(source='cached_to_user')
-    has_followed = serializers.SerializerMethodField()
+class FollowerSerializer(BaseFriendshipSerializer):
+    def get_user_id(self, obj):
+        return obj.from_user_id
 
-    class Meta:
-        model = Friendship
-        fields = ('user', 'created_at', 'has_followed')
 
-    def get_has_followed(self, obj):
-        if self.context['request'].user.is_anonymous:
-            return False
-        return obj.to_user_id in self.following_user_id_set
+class FollowingSerializer(BaseFriendshipSerializer):
+    def get_user_id(self, obj):
+        return obj.to_user_id
 
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
@@ -79,7 +82,7 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        friendship = Friendship.objects.create(
+        friendship = FriendshipsService.follow(
             from_user_id=validated_data['from_user_id'],
             to_user_id=validated_data['to_user_id'],
         )
--- friendships/services.py
+++ friendships/services.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.core.cache import caches
 from friendships.models import Friendship
 from twitter.cache import FOLLOWINGS_PATTERN
+from gatekeeper.models import GateKeeper
+from friendships.hbase_models import HBaseFollowing, HBaseFollower
+
+import time
 
 cache = caches['testing'] if settings.TESTING else caches['default']
 
@@ -53,3 +57,28 @@ class FriendshipsService(object):
     def invalidate_following_cache(cls, from_user_id):
         key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
         cache.delete(key)
+
+    @classmethod
+    def follow(cls, from_user_id, to_user_id):
+        if from_user_id == to_user_id:
+            return None
+
+        if not GateKeeper.is_switch_on('switch_friendship_to_hbase'):
+            # create friendship in mysql
+            return Friendship.objects.create(
+                from_user_id=from_user_id,
+                to_user_id=to_user_id,
+            )
+
+        # create data in hbase
+        now = int(time.time() * 1000000)
+        HBaseFollower.create(
+            from_user_id=to_user_id,
+            to_user_id=from_user_id,
+            created_at=now,
+        )
+        HBaseFollowing.create(
+            from_user_id=from_user_id,
+            to_user_id=to_user_id,
+            created_at=now,
+        )
